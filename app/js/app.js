52d6cb3a92ee0f2184ed5d091cbe846e2c72f11c
Haytham Akl <silents.coders@gmail.com>
2017-04-16 19:49:38 -0400

    add Winer Functional login

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,38 @@ let player_Symbol2 = '<span id="player_Symbol2">O</span>'
 let currentPlayer = player_Symbol
 let board = document.querySelector('.board')
 
+/*const Players = {
+  player: function (number){
+    this.number = number
+  }
+  addCss:=>(){
+
+  }
+}*/
+
+
 board.addEventListener('click',(e) => {
   e.target.innerHTML = currentPlayer
   currentPlayer = currentPlayer === player_Symbol ? player_Symbol2 : player_Symbol
 
+  whoWin() ? alert('You Won !') : ''
+
 })
+
+
+function whoWin(){
+  let Squares = Array.prototype.slice.call(document.querySelectorAll('.square'),0)
+  let Symbol = Squares.map((square)=>{
+    return square.textContent
+
+})
+var WininGnumbers = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+  return WininGnumbers.find((number) => {
+  if(Symbol[number[0]] == Symbol[number[1]] &&  Symbol[number[1]] ==  Symbol[number[2]]){
+    return Symbol[number[0]]
+  }else{
+    return false
+  }
+
+})}
